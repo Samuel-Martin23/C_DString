fb76e7f633aa2d942c69a2313d40531da10e5947
Samuel-Martin23 <59482608+Samuel-Martin23@users.noreply.github.com>
2021-08-27 14:51:19 -0700

    Whoops

--- main.c
+++ main.c
@@ -2,11 +2,19 @@
 
 int main()
 {
-    string_t *file = str_alloc_read_keyboard("\nHow are you doing today? ");
+    string_t *full_name = str_alloc( "  \nSamuel\n  ");
+    str_strip(full_name);
+    str_append(full_name, " Martin");
 
-    str_print(file, "", "\n");
+    int64_t start = 7;
+    string_t *last_name = str_alloc_substr(full_name, &start, 0, 0);
 
-    str_free(&file);
+    str_erase(full_name, "tin");
+    string_array_t *split_name = str_alloc_split(full_name, "a", 0);
+
+    str_free(&last_name);
+    str_free(&full_name);
+    str_array_free(&split_name);
 
     return 0;
 }
